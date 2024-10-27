b17f6bcdbff096a39e339f5795eda995a01a1396
Vincent Virgile DIMON <37027948+virgiledimon@users.noreply.github.com>
2024-10-27 14:02:13 +0000

    initialisation of the form

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,17 @@ st.write(
     "Explainable AI approach for impersonation attack detection in fog computing "
 )
 
-st.form("input_params_form", clear_on_submit=False, enter_to_submit=True, border=True)
 
-with st.form("my_form"):
-    st.write("Inside the form")
-    slider_val = st.slider("Form slider")
-    checkbox_val = st.checkbox("Form checkbox")
+with st.form("input_params_form", clear_on_submit=False, enter_to_submit=True, border=True):
+    st.write("Faites un choix de simulation")
+
+    algo = st.selectbox("Agent de détection", ["Double SARSA"])
+    xai_methode = st.selectbox("Méthode XAI", ["SHAP", "LIME", "Permutation Feature Importance"])
+    episode_nbr = st.number_input("Nombre d'épisode", min_value=30)
 
     # Every form must have a submit button.
-    submitted = st.form_submit_button("Submit")
+    submitted = st.form_submit_button("Simuler")
     if submitted:
-        st.write("slider", slider_val, "checkbox", checkbox_val)
-st.write("Outside the form")
+        st.write("episode_nbr", episode_nbr)
+
+st.write("Dernières séquences de détection")
