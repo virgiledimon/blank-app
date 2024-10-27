83a98d6b36ecb62c7bfbd5eee8b7d5811121129c
Vincent Virgile DIMON <37027948+virgiledimon@users.noreply.github.com>
2024-10-27 13:37:52 +0000

    initialisation of the form

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,16 @@ st.title("FogCID-XAI")
 st.write(
     "Explainable AI approach for impersonation attack detection in fog computing "
 )
+
+st.form("input_params_form", clear_on_submit=False, enter_to_submit=True, border=True)
+
+with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+st.write("Outside the form")
